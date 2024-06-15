34f6f5b3a205fd29386b6b8d6dd05de2fdd9a630
Jonathan Morales <jmoralesc54@miumg.edu.gt>
2024-06-15 07:49:17 -0600

    Carga de fuentes principal

---
+++ dist/coreui-free-angular-admin-template/browser/chunk-QMKRQMOL.js
@@ -0,0 +1,1 @@
+import"./chunk-ZG7TN3SX.js";var o=[{path:"",loadComponent:()=>import("./chunk-53SSUKUN.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
---
+++ dist/coreui-free-angular-admin-template/browser/chunk-QWVWWM6P.js
@@ -0,0 +1,1 @@
+import"./chunk-ZG7TN3SX.js";var t=[{path:"",loadComponent:()=>import("./chunk-6ZOC5KBY.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
